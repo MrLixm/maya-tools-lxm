ac9498a5a1d9eb370086caac7075e721a5295f6c
Liam Collod <lcollod@gmail.com>
2021-09-27 16:32:50 +0200

    Added license

--- src/cmds_interface_demo/maya.cmds_interface_demo.pizza_creator.py
+++ src/cmds_interface_demo/maya.cmds_interface_demo.pizza_creator.py
@@ -4,6 +4,10 @@ Require OOP skills.
 
 Author: Liam Collod
 Last Modified: 27-09-2021
+License:
+    This work is licensed under the Creative Commons Attribution 4.0 International License.
+    To view a copy of this license, visit http://creativecommons.org/licenses/by/4.0/
+    or send a letter to Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
 
 How To: 
     just run this script into maya script editor.
