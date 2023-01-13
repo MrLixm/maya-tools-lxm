b1bb13231ea48793307b406e532e00738ca4b85b
Liam Collod <lcollod@gmail.com>
2023-01-13 21:02:28 +0100

    [launchers.env...] updated userPrefs

--- src/launchers/env-default/userPrefs.py
+++ src/launchers/env-default/userPrefs.py
@@ -3,6 +3,7 @@ from maya import cmds
 cmds.optionVar(intValue=("showHomeScreenOnStartup", 0))
 cmds.optionVar(intValue=("viewCubeShowCube", 0))
 cmds.optionVar(intValue=("SafeModeUserSetupHashOption", 0))
+cmds.whatsNewHighlight(highlightOn=False, showStartupDialog=False)
 
 cmds.undoInfo(state=True, infinity=True)
 cmds.optionVar(intValue=("undoIsInfinite", 1))
@@ -24,8 +25,4 @@ cmds.optionVar(intValue=("playbackMaxRangeDefault", 1120))
 cmds.optionVar(intValue=("playbackMinDefault", 1001))
 cmds.optionVar(intValue=("playbackMinRangeDefault", 1001))
 
-# Disable the copy/paste "functionality" of Maya that wreaks havoc
-# WARNING: raise error, I don't know why. To put last.
-cmds.hotkeySet("NoCopyPaste", current=True)
-cmds.hotkey(keyShortcut="c", ctrlModifier=True, name="")
-cmds.hotkey(keyShortcut="v", ctrlModifier=True, name="")
+# TODO find a way to disable copy/pasting
