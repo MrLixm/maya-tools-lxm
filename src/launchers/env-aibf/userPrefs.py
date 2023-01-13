c4e863cfc952432b4de2e640f4f07bc435e69ac2
Liam Collod <lcollod@gmail.com>
2023-01-13 19:26:15 +0100

    [launchers] env-aibf: improved userPrefs

--- src/launchers/env-aibf/userPrefs.py
+++ src/launchers/env-aibf/userPrefs.py
@@ -1,5 +1,9 @@
 from maya import cmds
 
+cmds.optionVar(intValue=("showHomeScreenOnStartup", 0))
+cmds.optionVar(intValue=("viewCubeShowCube", 0))
+cmds.optionVar(intValue=("SafeModeUserSetupHashOption", 0))
+
 cmds.undoInfo(state=True, infinity=True)
 cmds.optionVar(intValue=("undoIsInfinite", 1))
 
@@ -13,6 +17,12 @@ cmds.optionVar(intValue=("useScenePanelConfig", 0))
 
 cmds.optionVar(intValue=("RecentFilesMaxSize", 10))
 cmds.optionVar(intValue=("RecentProjectsMaxSize", 10))
+
+# default framerange
+cmds.optionVar(intValue=("playbackMaxDefault", 1200))
+cmds.optionVar(intValue=("playbackMaxRangeDefault", 1120))
+cmds.optionVar(intValue=("playbackMinDefault", 1001))
+cmds.optionVar(intValue=("playbackMinRangeDefault", 1001))
 
 # Disable the copy/paste "functionality" of Maya that wreaks havoc
 # WARNING: raise error, I don't know why. To put last.
